f288c16e1ac0bd730ec2b468381d06d82935e214
Michał Szczurek <michalszczurek794@gmail.com>
2022-07-28 00:06:11 +0200

    MockOAuth2Provider tests

---
+++ backend/src/services/MockOAuth2Provider.test.ts
@@ -0,0 +1,26 @@
+import { OAuth2InvalidCodeError } from "../errors"
+import { MockOAuth2Provider } from "./OAuth2Providers"
+
+describe('Mock OAuth2 Provider tests', () => {
+    it('returns profile', async () => {
+        const provider = thereIsProvider()
+        const profile = await provider.exchange('abcdf')
+        expect(profile.id).toBe('abcdf')
+    })
+
+    it('can throw invalid code error', async () => {
+        const provider = thereIsProvider()
+        expect(provider.exchange('invalidcode')).rejects.toThrow(OAuth2InvalidCodeError)
+    })
+
+    it('doesnt change id but changes username everytime', async () => {
+        const provider = thereIsProvider()
+        const profile1 = await provider.exchange('abc')
+        expect(profile1.id).toBe('abc')
+        const profile2 = await provider.exchange('abc')
+        expect(profile2.id).toBe('abc')
+        expect(profile1.username === profile2.username).toBe(false)
+    })
+})
+
+const thereIsProvider = () => new MockOAuth2Provider()
